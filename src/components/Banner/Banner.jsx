c8786d11dc0d019424d6c6c3d84eeaff5e31c392
shahroz <shahroz@gmail.com>
2024-05-15 19:13:26 +0500

    banner component completed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 import './Banner.css'
+import hand_icon from "../Assets/hand_icon.png"
+import arrow_icon from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image.png'
 export const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className='banner'>
+        <div className="banner-left">
+            <h2>NEW ARRIVALS ONLY</h2>
+            <div>
+                <div className='banner-hand-icon'>
+                    <p>new</p>
+                    <img src={hand_icon} alt="" />
+                </div>
+                <p>collections</p>
+                <p>for everyone</p>
+            </div>
+            <div className="banner-latest-btn">
+                <div>Latest Collection </div>
+                <img src={arrow_icon} alt="" />
+            </div>
+        </div>
+        <div className="banner-right">
+            <img src={hero_image} alt="" />
+        </div>
+    </div>
   )
 }
