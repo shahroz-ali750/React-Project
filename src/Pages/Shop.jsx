11456cf3c694a79cb77e5793241cd2c820230187
shahroz <shahroz@gmail.com>
2024-05-17 21:01:37 +0500

    New collections component completed

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { Banner } from '../components/Banner/Banner'
-import { Popular } from '../components/Popular/Popular'
-import Offers from '../components/Offers/Offers'
-
+import React from "react";
+import { Banner } from "../components/Banner/Banner";
+import { Popular } from "../components/Popular/Popular";
+import Offers from "../components/Offers/Offers";
+import Newcollections from "../components/NewCollections/Newcollections";
 export const Shop = () => {
   return (
     <div>
-      <Banner/>
-      <Popular/>
+      <Banner />
+      <Popular />
       <Offers />
+      <Newcollections/>
     </div>
-  )
-}
+  );
+};
